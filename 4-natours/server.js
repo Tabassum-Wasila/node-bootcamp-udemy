7a74b11767418c5246da962409907f24ddfa7e6b
Tabassum-Wasila <twahsila@gmail.com>
2024-02-09 23:28:47 +1100

    Configured basic ENV variables for development and production

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,5 +1,9 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
+console.log(process.env.NODE_ENV);
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
