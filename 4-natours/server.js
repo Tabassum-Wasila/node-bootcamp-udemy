178801d058c8b79ebfe68822a40f7e9528405a0d
Tabassum-Wasila <twahsila@gmail.com>
2024-02-16 19:25:22 +1100

    Added mongoose configs and tour schema

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,10 +1,32 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
 dotenv.config({ path: './config.env' });
 
-const app = require('./app');
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DB_PASSWORD);
+
+mongoose.connect(DB).then(() => {
+  console.log('DB connection successful!');
+});
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+const Tour = mongoose.model('Tour', tourSchema);
 
 console.log(process.env.NODE_ENV);
-const port = 3000;
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
